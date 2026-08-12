e58c94b6db6129a5816f7e93d092124c12946502
Sahilpreet Singh <spsk1699@gmail.com>
2026-08-11 23:00:11 -0400

    chore: initialize expense budgeting project

---
+++ src/main/java/com/spsk1313/expensebudgeting/ExpenseBudgetingApplication.java
@@ -0,0 +1,13 @@
+package com.spsk1313.expensebudgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseBudgetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseBudgetingApplication.class, args);
+    }
+
+}
